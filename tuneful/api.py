3face65321810774312f293f1217a3d42c80d157
smedinacorey <s.medinacorey@gmail.com>
2017-01-03 04:15:08 +0000

    added get and post functionality, and get tests. post tests forthcoming

--- tuneful/api.py
+++ tuneful/api.py
@@ -9,6 +9,44 @@ from . import models
 from . import decorators
 from tuneful import app
 from .database import session
+from .models import Song, File
 from .utils import upload_path
 
 
+song_schema = {
+    "properties": {
+        "file": {
+            "id": {"type": "int"},
+            "name": {"type": "string"}
+        }
+    }
+}
+
+
+@app.route("/api/songs", methods=["GET"])
+def songs_get():
+    """Get a list of songs as JSON"""
+    
+    songs = session.query(Song).all()
+    songs = songs.order_by(Song.id)
+    
+    data = json.dumps([song.as_dictionary() for song in songs])
+    return Response(data, 200, mimetype="application/json")
+    
+@app.route("/api/songs", methods=["POST"])
+def songs_post():
+    """Add a new song to the db"""
+    
+    data = request.json
+    
+    try: 
+        validate(data, song_schema)
+    except ValidationError as error:
+        data = {"message": error.message}
+        return Response(json.dumps(data), 422, mimetype="application/json")
+        
+    song = Song(name=data["file.name"], file_id=data["file.id"])
+    session.add(song)
+    session.commit()
+    
+    
--- tuneful/models.py
+++ tuneful/models.py
@@ -7,3 +7,41 @@ from sqlalchemy.orm import relationship
 from tuneful import app
 from .database import Base, engine
 
+
+
+class File(Base):
+    __tablename__ = "file"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    
+    song = relationship("Song", uselist=False, backref="file")
+    
+    def as_dictionary(self):
+        file = {
+            "id": self.id,
+            "name": self.name,
+        }
+        
+class Song(Base):
+    __tablename__ = "song"
+    
+    id = Column(Integer, primary_key=True)
+    
+    file_id = Column(Integer, ForeignKey="file.id", nullable=False)
+    
+    def as_dictionary(self):
+        song = {
+            "id": self.id,
+            "file": {
+                "id": self.file.id,
+                "name": self.file.name
+            }
+        }
+        
+        
+    # file.song.id 
+    # song.file.id
+    # song.file.name
+    # song.id
+    # file.id
